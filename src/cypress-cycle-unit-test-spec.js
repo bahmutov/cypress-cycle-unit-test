4f30d7084b0e99ed44685a34bd72ff52e54adf62
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-02-13 23:23:20 -0500

    chore: initial code

---
+++ src/cypress-cycle-unit-test-spec.js
@@ -0,0 +1,10 @@
+'use strict'
+
+/* eslint-env mocha */
+const cypressCycleUnitTest = require('.')
+
+describe('cypress-cycle-unit-test', () => {
+  it('write this test', () => {
+    console.assert(cypressCycleUnitTest, 'should export something')
+  })
+})
